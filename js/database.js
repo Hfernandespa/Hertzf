6a456e5eaaa6e310b716cd3d2ee1530a8e456903
Hfernandespa <healeyfernandes@hotmail.com>
2025-10-03 11:12:56 -0300

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,7 @@
+// js/database.js
+const frequenciasDB = {
+    // ... Cole todo o objeto 'frequenciasDB' aqui ...
+};
+
+// Torna o banco de dados disponível globalmente para outros scripts
+window.frequenciasDB = frequenciasDB;
